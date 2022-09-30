cfafcebaf15ea15acd31a8a3996a056b85076065
Grace Todd <toddgr@oregonstate.edu>
2022-09-29 22:08:32 -0700

    Set up compare function

---
+++ guessing_game/target/debug/deps/guessing_game-bbd1b4e43c50b573.d
@@ -0,0 +1,5 @@
+C:\Users\Grace\Desktop\Rust-Guessing-Game\guessing_game\target\debug\deps\guessing_game-bbd1b4e43c50b573.exe: src\main.rs
+
+C:\Users\Grace\Desktop\Rust-Guessing-Game\guessing_game\target\debug\deps\guessing_game-bbd1b4e43c50b573.d: src\main.rs
+
+src\main.rs:
